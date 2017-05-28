bef5f066cfea8d6e42ff9d695021473e79815590
Nilesh PS <nps17thatsme@gmail.com>
2017-05-28 14:11:40 -0400

    Trying out new transform equation.

--- app/src/main/java/com/sample/MyTransformer.java
+++ app/src/main/java/com/sample/MyTransformer.java
@@ -19,42 +19,41 @@ final Float ORG_POS = 0f;
 
     @Override
     public void transformPage(View page, float position) {
+//         TextView tv = (TextView) page.findViewById(R.id.text);
+//         float prev_pos;
+//         Log.d("POS2", tv.getText().toString() + "  " + String.valueOf(position) + " Y: " + String.valueOf(page.getY()));
+//         if (position <=  .10f){
+//             page.setScaleY(.7f);
+//             page.setScaleX(.7f);
+//             page.setTranslationY(150f);
+//         }
+//         else  if (position >= .60f){
+//             page.setScaleY(.7f);
+//             page.setScaleX(.7f);
+//             page.setTranslationY(150f);
+//         }
+//         else{
 
+//             page.setScaleY(1f);
+//             page.setScaleX(1f);
+//             page.setTranslationY(0f);
+//         }
 
-
-        TextView tv = (TextView) page.findViewById(R.id.text);
-
-        float prev_pos;
-
-        Log.d("POS2", tv.getText().toString() + "  " + String.valueOf(position) + " Y: " + String.valueOf(page.getY()));
-
-
-
-        if (position <=  .10f){
-
-            page.setScaleY(.7f);
-            page.setScaleX(.7f);
-
-            page.setTranslationY(150f);
+        float left = -0.3333f;
+        if(position < left) {
+            page.setTranslationY(0);
+            return;
         }
-
-        else  if (position >= .60f){
-
-            page.setScaleY(.7f);
-            page.setScaleX(.7f);
-            page.setTranslationY(150f);
+        final float bottomWidth = 250;
+        float positivePosition = (position - left) / (1.3333f), x, c = 50;
+        if(positivePosition <= 0.5f) {
+            x = positivePosition * bottomWidth;
+        } else {
+            x = (1 - positivePosition ) * bottomWidth;
         }
-
-        else{
-
-            page.setScaleY(1f);
-            page.setScaleX(1f);
-            page.setTranslationY(0f);
-
-
-
-
-        }
+        float fourA = 100f;
+        float y = c - (float)Math.pow(x, 2) / fourA ;
+        page.setTranslationY(y - page.getTop());
 
 
 
